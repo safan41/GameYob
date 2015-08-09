31959975f8b128b61b10b0fe8c79220e2eb01ade
Steven Smith <Steveice10@gmail.com>
2015-08-08 22:02:21 -0700

    Change default FF Frame Skip value to 2.

--- source/platform/common/menu.cpp
+++ source/platform/common/menu.cpp
@@ -648,7 +648,7 @@ SubMenu menuList[] = {
                         {"Game Screen", setScreenFunc, 2, {"Top", "Bottom"}, 0, MENU_ALL},
                         {"Scaling", setScaleModeFunc, 5, {"Off", "125%", "150%", "Aspect", "Full"}, 0, MENU_ALL},
                         {"Scale Filter", setScaleFilterFunc, 3, {"Nearest", "Linear", "Scale2x"}, 1, MENU_ALL},
-                        {"FF Frame Skip", setFastForwardFrameSkipFunc, 4, {"0", "1", "2", "3"}, 0, MENU_ALL},
+                        {"FF Frame Skip", setFastForwardFrameSkipFunc, 4, {"0", "1", "2", "3"}, 3, MENU_ALL},
                         {"Colorize GB", gbColorizeFunc, 14, {"Off", "Auto", "Inverted", "Pastel Mix", "Red", "Orange", "Yellow", "Green", "Blue", "Brown", "Dark Green", "Dark Blue", "Dark Brown", "Classic Green"}, 1, MENU_ALL},
                         {"Borders", borderFunc, 3, {"Off", "Custom", "SGB"}, 1, MENU_ALL},
                         {"Select Border", selectBorderFunc, 0, {}, 0, MENU_ALL}
