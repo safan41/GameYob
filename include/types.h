a624b34ed5aa42770bbd073d703a2d12867d38b8
Steven Smith <Steveice10@gmail.com>
2016-07-14 13:56:59 -0700

    Merge RGB conversion functions.

--- include/types.h
+++ include/types.h
@@ -16,18 +16,14 @@ typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
 
-inline u32 RGB555ComponentsToRGB8888(u8 r5, u8 g5, u8 b5) {
-    u8 r8 = ((r5 << 3) | (r5 >> 2));
-    u8 g8 = ((g5 << 3) | (g5 >> 2));
-    u8 b8 = ((b5 << 3) | (b5 >> 2));
-
-    return (u32) (r8 << 24 | g8 << 16 | b8 << 8 | 0xFF);
-}
-
 inline u32 RGB555ToRGB8888(u16 rgb555) {
     u8 r5 = (u8) (rgb555 & 0x1F);
     u8 g5 = (u8) ((rgb555 >> 5) & 0x1F);
     u8 b5 = (u8) ((rgb555 >> 10) & 0x1F);
 
-    return RGB555ComponentsToRGB8888(r5, g5, b5);
+    u8 r8 = ((r5 << 3) | (r5 >> 2));
+    u8 g8 = ((g5 << 3) | (g5 >> 2));
+    u8 b8 = ((b5 << 3) | (b5 >> 2));
+
+    return (u32) (r8 << 24 | g8 << 16 | b8 << 8 | 0xFF);
 }
